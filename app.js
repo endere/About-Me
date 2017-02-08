1cf49391f7875f96436e61046164ab5251b6ae55
Erik Enderlein <erik.end@gmail.com>
2017-02-08 15:18:33 -0600

    first update

---
+++ app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var confirmquiz = confirm('Hello, would you like to play a game?');
+
+if(confirmquiz) {
+  console.log('Awsome! Lets get started!');
+  alert('Awesome! Lets get started!' );
+} else {
+  console.log('Too Bad, starting anyway.');
+  alert('Too bad, starting anyway' );
+}
+
+var quizResponseOne = prompt('How many pets do I have?');
+
+if(quizResponseOne === '2' || quizReponseOne === 'two') {
+  console.log('You are correct!');
+  alert('You are correct!' );
+} else {
+  console.log('Too Bad, you are wrong. I have two pets.');
+  alert('Too bad, you are wrong. I have two pets.' );
+}
+
+var quizResponseTwo = prompt('Please Answer with Y or N.');
+
+if(quizResponseTwo.toUpperCase()) {
+  console.log('You are correct!');
+  alert('You are correct!' );
+} else {
+  console.log('Too Bad, you are wrong.');
+  alert('Too bad, you are wrong' );
+}
+
+var quizResponseOne = prompt('Please Answer with Y or N.');
+
+if(quizResponseOne.toUpperCase()) {
+  console.log('You are correct!');
+  alert('You are correct!' );
+} else {
+  console.log('Too Bad, you are wrong.');
+  alert('Too bad, you are wrong' );
+}
+
+var quizResponseOne = prompt('Please Answer with Y or N.');
+
+if(quizResponseOne.toUpperCase()) {
+  console.log('You are correct!');
+  alert('You are correct!' );
+} else {
+  console.log('Too Bad, you are wrong.');
+  alert('Too bad, you are wrong' );
+}
+
+var quizResponseOne = prompt('Please Answer with Y or N.');
+
+if(quizResponseOne.toUpperCase()) {
+  console.log('You are correct!');
+  alert('You are correct!' );
+} else {
+  console.log('Too Bad, you are wrong.');
+  alert('Too bad, you are wrong' );
+}
