ceaf091e0423a7d46e887519f18fb870522f8b08
Isak Swearingen <isakswearingen@gmail.com>
2017-02-10 15:17:39 -0800

    Pair work with Isak, put program logic into functions

--- app.js
+++ app.js
@@ -6,6 +6,51 @@ console.log('The correct number for question 6 is ' + number);
 var numbertries = 4;
 var animaltries = 6;
 var animals = ['fox','whale','sheep','dove','otter'];
+function askYN(i){
+  var response = prompt(questions[i]);
+  if (response.toUpperCase() === correctAnswers[i]){
+    console.log(corrects[i]);
+    alert(corrects[i]);
+    correctcount++;
+  }
+  else if (response.toUpperCase() === wrongAnswers[i]){
+    console.log(wrongs[i]);
+    alert(wrongs[i]);
+  } else {
+    console.log('invalid input');
+    alert('Please input either a Y or an N.');
+    i--;
+  }
+}
+function askRandom(i){
+  var response = prompt(questions[i]);
+  if(+response === correctAnswers[i]){
+    console.log(response);
+    console.log(corrects[i]);
+    alert(corrects[i]);
+    correctcount++;
+  } else if (numbertries > 1) {
+    numbertries--;
+    console.log(numbertries + " tries remaining.");
+    alert("You have " + numbertries + ' tries remaining.');
+    i--;
+  }
+}
+
+function askAnimal(i){
+  var response = prompt(questions[i]);
+  if (animals.includes(response.toLowerCase())){
+    alert(corrects[i]);
+    correctcount++;
+  }else if (animaltries > 1) {
+    animaltries--;
+    alert('You have ' + animaltries + ' tries remaining.');
+    i--;
+  }else{
+    alert('My favorite animals are: \n fox \n whale \n sheep \n dove \n otter');
+  }
+
+}
 
 var questions = ['Do I have any pets?', 'Is blue my favorite color?', 'Am I from Seattle?',  'Am I under six feet tall?', 'Is the rhombicosidodecahedron my favorite Archimedean solid?','I am thinking of a number between 1 and 10... You get 4 tries...', 'What is one of my favorite animals? You have 6 tries!' ];
 var correctAnswers = ['Y', 'Y', 'Y', 'N', 'N', number];
@@ -22,9 +67,15 @@ if(confirmquiz) {
   alert('Hello, ' + name + ', Please answer all yes or no questions with a Y or N.');
 
   for (var i = 0; i < questions.length; i++) {
-    var response = prompt(questions[i]);
     if (i < 5){
-      if (response.toUpperCase() === correctAnswers[i]){
+      askYN(i);
+    }
+    if(i === 5){
+      askRandom(i);
+    }else if (i === 6){
+      askAnimal(i);
+    }
+      /*if (response.toUpperCase() === correctAnswers[i]){
         console.log(corrects[i]);
         alert(corrects[i]);
         correctcount++;
@@ -64,6 +115,7 @@ if(confirmquiz) {
       }
 
     }
+    */
   }
 
   alert(name + ', you have guessed ' + correctcount + ' out of 7 correctly! Thanks for playing!');
