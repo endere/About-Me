37a270a53570bb863c7ede4e2dddd379af4f7ca1
Erik Enderlein <erik.end@gmail.com>
2017-02-10 18:57:03 -0600

    removed commented scrap code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ console.log('The correct number for question 6 is ' + number);
 var numbertries = 4;
 var animaltries = 6;
 var animals = ['fox','whale','sheep','dove','otter'];
+
 function askYN(i){
   var response = prompt(questions[i]);
   if (response.toUpperCase() === correctAnswers[i]){
@@ -31,8 +32,8 @@ function askRandom(i){
     correctcount++;
   } else if (numbertries > 1) {
     numbertries--;
-    console.log(numbertries + " tries remaining.");
-    alert("You have " + numbertries + ' tries remaining.');
+    console.log(numbertries + 'tries remaining.');
+    alert('You have ' + numbertries + ' tries remaining.');
     i--;
   }
 }
@@ -52,7 +53,7 @@ function askAnimal(i){
 
 }
 
-var questions = ['Do I have any pets?', 'Is blue my favorite color?', 'Am I from Seattle?',  'Am I under six feet tall?', 'Is the rhombicosidodecahedron my favorite Archimedean solid?','I am thinking of a number between 1 and 10... You get 4 tries...', 'What is one of my favorite animals? You have 6 tries!' ];
+var questions = ['Do I have any pets?', 'Is blue my favorite color?', 'Am I from Seattle?', 'Am I under six feet tall?', 'Is the rhombicosidodecahedron my favorite Archimedean solid?','I am thinking of a number between 1 and 10... You get 4 tries...', 'What is one of my favorite animals? You have 6 tries!' ];
 var correctAnswers = ['Y', 'Y', 'Y', 'N', 'N', number];
 var corrects = [ 'You are correct! I have two kitties!', 'You are correct! It is my favorite color!', 'You are correct! I am from Seattle!', 'That\'s right! This was an easy one...', 'You are correct! I like the rhombicuboctahedron a lot more!', 'Good job! you got it right!','You got one of them! My favorite animals are: \n fox \n whale \n sheep \n dove \n otter'];
 
@@ -76,50 +77,7 @@ if(confirmquiz) {
       askAnimal(i);
     }
   }
-
   alert(name + ', you have guessed ' + correctcount + ' out of 7 correctly! Thanks for playing!');
 }else{
   alert('Okay! Here is my site!');
 }
-
-      /*if (response.toUpperCase() === correctAnswers[i]){
-        console.log(corrects[i]);
-        alert(corrects[i]);
-        correctcount++;
-      }
-      else if (response.toUpperCase() === wrongAnswers[i]){
-        console.log(wrongs[i]);
-        alert(wrongs[i]);
-      } else {
-        console.log('invalid input');
-        alert('Please input either a Y or an N.');
-        i--;
-      }
-    }
-    if(i === 5){
-      if(+response === correctAnswers[i]){
-        console.log(response);
-        console.log(corrects[i]);
-        alert(corrects[i]);
-        correctcount++;
-      } else if (numbertries > 1) {
-        numbertries--;
-        console.log(numbertries + " tries remaining.");
-        alert("You have " + numbertries + ' tries remaining.');
-        i--;
-      }
-    }
-    else if (i === 6){
-      if (animals.includes(response.toLowerCase())){
-        alert(corrects[i]);
-        correctcount++;
-      }else if (animaltries > 1) {
-        animaltries--;
-        alert('You have ' + animaltries + ' tries remaining.');
-        i--;
-      }else{
-        alert('My favorite animals are: \n fox \n whale \n sheep \n dove \n otter');
-      }
-
-    }
-    */
